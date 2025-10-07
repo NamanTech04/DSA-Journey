244ae97a2f6138e9f1f102db3be7c65650f664fd
Naman vyas <namanvyas2004@gmail.com>
2025-10-07 18:18:55 +0530

    Bottom View of Binary Tree

---
+++ gfg/Bottom View of Binary Tree.java
@@ -0,0 +1,54 @@
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.TreeMap;
+// import org.w3c.dom.Node; // Removed incorrect import
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+}
+
+class Solution {
+
+    static class Pair {
+        Node node;
+        int hd;
+        Pair(Node node, int hd) {
+            this.node = node;
+            this.hd = hd;
+        }
+    }
+
+    public ArrayList<Integer> bottomView(Node root) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        if (root == null) return ans;
+
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        Queue<Pair> q = new LinkedList<>();
+
+        q.add(new Pair(root, 0));
+
+        while (!q.isEmpty()) {
+            Pair curr = q.poll();
+            Node node = curr.node;
+            int hd = curr.hd;
+
+            map.put(hd, node.data);
+
+            if (node.left != null)
+                q.add(new Pair(node.left, hd - 1));
+
+            if (node.right != null)
+                q.add(new Pair(node.right, hd + 1));
+        }
+
+        for (int val : map.values())
+            ans.add(val);
+
+        return ans;
+    }
+}
