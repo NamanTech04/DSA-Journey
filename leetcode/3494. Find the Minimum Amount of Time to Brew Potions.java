145530755f91caa142eb38e0da5112781a7c12a9
Naman vyas <namanvyas2004@gmail.com>
2025-10-09 14:15:35 +0530

    3494. Find the Minimum Amount of Time to Brew Potions

---
+++ leetcode/3494. Find the Minimum Amount of Time to Brew Potions.java
@@ -0,0 +1,21 @@
+
+import java.util.Arrays;
+
+class Solution {
+    @SuppressWarnings("unused")
+    long minTime(int[] skill, int[] mana) {
+    long[] endTime;
+        endTime = new long[skill.length];
+    Arrays.fill(endTime, 0);
+    for(int i = 0; i < mana.length; ++i){
+        long t = 0, maxDiff = 0;
+        for(int j = 0; j < skill.length; ++j){
+            maxDiff = Math.max(maxDiff, endTime[j] - t);
+            t += (long)skill[j] * (long)mana[i];
+            endTime[j] = t;
+        }
+        for(int j = 0; j < skill.length; ++j) endTime[j] += maxDiff;
+    }
+    return endTime[endTime.length-1];
+}
+}
